423985d9eb42bacb1a1e8fce88ce691cebd481a8
Evgen2308 <107253440+Evgen2308@users.noreply.github.com>
2023-03-13 15:05:41 +0200

    2-nd edition

--- project/project/settings.py
+++ project/project/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
--- project/simpleapp/views.py
+++ project/simpleapp/views.py
@@ -3,18 +3,27 @@ from django.shortcuts import render
 # Create your views here.
 # Импортируем класс, который говорит нам о том,
 # что в этом представлении мы будем выводить список объектов из БД
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
-
+from datetime import datetime
 
 class ProductsList(ListView):
-    # Указываем модель, объекты которой мы будем выводить
     model = Product
-    # Поле, которое будет использоваться для сортировки объектов
     ordering = 'name'
-    # Указываем имя шаблона, в котором будут все инструкции о том,
-    # как именно пользователю должны быть показаны наши объекты
     template_name = 'products.html'
-    # Это имя списка, в котором будут лежать все объекты.
-    # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
+    paginate_by = 2
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['time_now'] = datetime.utcnow()
+        context['next_sale'] = "Распродажа в среду!"
+        return context
+
+class ProductDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Product
+    # Используем другой шаблон — product.html
+    template_name = 'product.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'product'
