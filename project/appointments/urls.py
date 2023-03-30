f7db14ed029059276939a0a69f46a32e555f0101
Evgen2308 <107253440+Evgen2308@users.noreply.github.com>
2023-03-30 14:22:35 +0200

    Add files via upload

--- project/appointments/urls.py
+++ project/appointments/urls.py
@@ -4,4 +4,6 @@ from .views import AppointmentView
 
 urlpatterns = [
    path('', AppointmentView.as_view(), name='appointments'),
+   path('make_appointment', AppointmentView.as_view(), name='make_appointment'),
+   path('<int:pk>', AppointmentView.as_view(), name='appointment_create'),
 ]
